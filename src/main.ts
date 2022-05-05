bb055ee9e5b21351793906374c156cc277a2a4e2
Dzmitry Yarmoshkin <spanb4@gmail.com>
2022-05-05 08:17:52 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ const LOG_CONSOLE = JSON.parse(process.env.LOG_CONSOLE as string);
 async function _initApp(isFastify: boolean, isLogger: boolean) {
   if (isFastify) {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: isLogger }));
-    app.enableCors();
+    app.enableCors({ origin: 'http://localhost:3000', credentials: true });
     app.register(contentParser);
     return app;
   }
